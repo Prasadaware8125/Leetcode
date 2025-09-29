9654348c6447eee848b0c92f678a994202d4eab4
Prasad Aware <prasadaware6613@gmail.com>
2025-09-29 21:29:57 +0530

    Time: 4 ms (55.34%) | Memory: 43.3 MB (49.35%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,43 +1,31 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        // Remove duplicates first
-        int[] unique = java.util.Arrays.stream(nums).distinct().toArray();
-        
-        if (unique.length < 3) {
-            // Return the maximum
-            int max = unique[0];
-            for (int x : unique) max = Math.max(max, x);
-            return max;
-        }
-        
-        // Otherwise find 3rd max using QuickSelect
-        return select(unique, unique.length, 3);
-    }
+        Integer first = null;
+        Integer second = null;
+        Integer third = null;
+        int res[] = new int[3];
 
-    public int select(int[] a, int n, int k) {
-        int low = 0, up = n;
-        while (low < up) {
-            int j = partition(a, low, up);
-            int rank = j - low + 1;
-            if (rank == k) return a[j];
-            else if (rank < k) { k -= rank; low = j + 1; }
-            else up = j;
-        }
-        return -1;
-    }
+        for( Integer num : nums ) {
 
-    public int partition(int[] a, int m, int p) {
-        int v = a[m], i = m, j = p - 1;
-        while (i < j) {
-            while (i < p && a[i] >= v) i++;
-            while (a[j] < v) j--;
-            if (i < j) interchange(a, i, j);
-        }
-        interchange(a, m, j);
-        return j;
-    }
+            if( num.equals(first) || num.equals(second) || num.equals(third) ) {
+                continue;
+            }   // For overcome the repetions or duplicate values
 
-    public void interchange(int[] a, int i, int j) {
-        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+            if( first == null || first < num ) {
+                third = second;
+                second = first;
+                first = num;
+            } else if( second == null || second < num ) {
+                third = second;
+                second = num;
+            } else if( third == null || third < num ) {
+                third = num;
+            }
+        }
+        if( third == null ) {
+            return first;
+        }
+        // return third == Integer.MIN_VALUE ? (int) first : (int) third;
+        return third;
     }
 }
