6ce1b2dca26385e5ce6bacf1183d7114965b0301
Prasad Aware <prasadaware6613@gmail.com>
2025-12-05 10:09:27 +0530

    Time: 1 ms (48.14%) | Memory: 43.5 MB (39.63%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,27 +1,22 @@
 class Solution {
     public int countPartitions(int[] nums) {
-        // Forward sum
-        int prefix[] = new int[nums.length];
-        prefix[0] = nums[0];
-        for( int i = 1 ; i < nums.length ; i++  ) {
-            prefix[i] = prefix[i-1] + nums[i];
+        int sum = 0;
+        for( int num : nums ) {
+            sum += num;
         }
-        if( prefix[nums.length-1] % 2 != 0 ) {
+
+        if( sum % 2 != 0 ) {
             return 0;
         }
-        // Backward sum
-        int suffix[] = new int[nums.length];
-        suffix[nums.length-1] = nums[nums.length-1];
-        for( int i = nums.length-2 ; i >= 0 ; i-- ) {
-            suffix[i] = suffix[i+1] + nums[i];       
-        }
 
-        int sum =0, count =0;
-        for(int i = 0 ; i < nums.length-1 ; i++ ) {
-            sum = prefix[i]+suffix[i+1];
-            if(sum %2 == 0) count++;    // even sum
+        int currSum = 0, count = 0, nextArr = 0;
+        for( int i = 0 ; i < nums.length ; i++ ) {
+            currSum += nums[i];
+            nextArr = sum - currSum;
+            if( (currSum + nextArr)  % 2 == 0 ) {
+                count++;
+            }
         }
-
-        return count;
+        return count-1;
     }
 }
