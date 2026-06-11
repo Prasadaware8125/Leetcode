4e22dabb0f432b014a1cd94c87c72d5b98454194
Prasad Aware <prasadaware6613@gmail.com>
2026-06-11 23:02:09 +0530

    Time: 4 ms (99.72%) | Memory: 47.9 MB (11.80%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,42 +1,21 @@
 class Solution {
-    public boolean check(String s) {
-        boolean change = false;
-        int l = 0, r = s.length()-1;
-        while( l <= r ) {
-            if( s.charAt(l) == s.charAt(r) ) {
-                l++;
-                r--;
-            } else if( change == false && s.charAt(l) == s.charAt(r-1) ) {
-                r--;
-                change = true;
-            } else if( change == false && s.charAt(l+1) == s.charAt(r) ) {
-                l++;
-                change = true;
-            } else {
-                return false;
-            }
-        }
-
-        return true;
-    }
     public boolean validPalindrome(String s) {
-        boolean change = false;
-        int l = 0, r = s.length()-1;
-        while( l <= r ) {
-            if( s.charAt(l) == s.charAt(r) ) {
-                l++;
-                r--;
-            } else if( change == false && s.charAt(l+1) == s.charAt(r) ) {
-                l++;
-                change = true;
-            } else if( change == false && s.charAt(l) == s.charAt(r-1) ) {
-                r--;
-                change = true;
+        return checkPalindrome(s, 0, s.length()-1, true); 
+    }
+
+    public boolean checkPalindrome(String s, int i, int j, boolean flag) {
+        while( i < j ) {
+            if( s.charAt(i) != s.charAt(j) ) {
+                if( flag == true ) {
+                    return checkPalindrome(s, i+1, j, false) || checkPalindrome(s, i, j-1, false);
+                } else {
+                    return false;
+                }
             } else {
-                return check(s);
+                i++;
+                j--;
             }
         }
-
         return true;
     }
 }
