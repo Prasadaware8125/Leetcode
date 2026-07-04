a04cee1118c7e4b77aef81bca2adef7357e0e656
Prasad Aware <prasadaware6613@gmail.com>
2026-07-05 00:32:15 +0530

    Time: 3 ms (99.41%) | Memory: 46.7 MB (66.94%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,46 +1,68 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int n = numCourses;
-        ArrayList<Integer>[] graph = new ArrayList[n];
-        for( int i = 0 ; i < n ; i++ ) {
+    static class Edge{
+        int src, dest;
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public void create_graph(int[][] prerequisites, ArrayList<Edge>[] graph) {
+        for( int i = 0 ; i < graph.length ; i++ ) {
             graph[i] = new ArrayList<>();
         }
-        int[] inDeg = new int[n];
 
-        for( int[] pre : prerequisites ) {
-            int course = pre[0];
-            int prereq = pre[1];
+        for( int i = 0 ; i < prerequisites.length ; i++ ) {
+            int d = prerequisites[i][0];
+            int s = prerequisites[i][1];
 
-            graph[prereq].add(course);
-            inDeg[course]++;
+            graph[s].add(new Edge(s, d));
+        }
+    }
+    public boolean topSort(ArrayList<Edge>[] graph, boolean[] vis, Stack<Integer> s, boolean[] st, int curr) {
+        vis[curr] = true;
+        st[curr] = true;
+        
+        for( int i = 0 ; i < graph[curr].size() ; i++ ) {
+            Edge e = graph[curr].get(i);
+            if( st[e.dest] ) {
+                return true;
+            }
+            if( vis[e.dest] == false ) {
+                if(topSort(graph, vis, s, st, e.dest)) {
+                    return true;
+                }
+            }
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for( int i = 0 ; i < n ; i++ ) {
-            if( inDeg[i] == 0 ) {
-                q.add(i);
+
+        s.push(curr);
+        st[curr] = false;
+        return false;
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        create_graph(prerequisites, graph);
+        boolean[] vis = new boolean[n];
+        boolean[] st = new boolean[n];
+        Stack<Integer> s = new Stack<>();
+        
+        for( int i = n-1 ; i >= 0 ; i-- ) {
+            if( vis[i] == false ) {
+                if(topSort(graph, vis, s, st, i)) {
+                    return new int[0];
+                }
             }
         }
 
         int[] ans = new int[n];
         int idx = 0;
-
-        while( !q.isEmpty() ) {
-            int curr = q.remove();
+        while (!s.isEmpty() ) {
+            int curr = s.pop();
             ans[idx++] = curr;
-            inDeg[curr]--;
-            for( int next : graph[curr] ) {
-                inDeg[next]--;
-                if( inDeg[next] == 0 ) {
-                    q.add(next);
-                }
-            }
         }
 
-        if( idx != n ) {
-            return new int[0];
-        }
-        
         return ans;
     }
 }
