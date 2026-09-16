122b0d6d16a8bd8af807e20ad13be661b0887a34
Prasad Aware <prasadaware6613@gmail.com>
2026-09-16 22:43:57 +0530

    Time: 15 ms (59.90%) | Memory: 51.4 MB (55.78%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int count = 0;
+        for( int j = 0 ; j  < s.length ; j++ ) {
+            if( i < g.length && g[i] <= s[j] ) {
+                count++;
+                i++;
+            }
+        }
+        return count;
+    }
+}
